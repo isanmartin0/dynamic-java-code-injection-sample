20d678b01b78c06c20b25d78731456b963d25f6a
isanmartin <isanmartin@keedio.com>
2018-10-09 14:59:45 +0200

    Added invocation examples with map (javascript)

--- src/main/java/org/keedio/javascript/DynamicJavascriptCaller.java
+++ src/main/java/org/keedio/javascript/DynamicJavascriptCaller.java
@@ -4,6 +4,8 @@ import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DynamicJavascriptCaller {
 
@@ -21,12 +23,22 @@ public class DynamicJavascriptCaller {
         engine.eval(new FileReader(fileJsPath));
         Invocable invocable = (Invocable) engine;
 
+        System.out.println("\nExample function call with single parameters");
         Object result = invocable.invokeFunction(functionName, a, b);
         System.out.println(result);
         System.out.println(result.getClass());
 
+        System.out.println("\nExample function call with array");
         int[] arrayValues = {1,2,3,4,5};
         result = invocable.invokeFunction("myComputeArray", arrayValues);
+        System.out.println(result);
+        System.out.println(result.getClass());
+
+        System.out.println("\nExample function call with map");
+        Map<String, Integer> valuesMap = new HashMap<>();
+        valuesMap.put("par1", new Integer(a));
+        valuesMap.put("par2", new Integer(b));
+        result = invocable.invokeFunction("fun2", valuesMap);
         System.out.println(result);
         System.out.println(result.getClass());
 
--- src/main/resources/script.js
+++ src/main/resources/script.js
@@ -14,4 +14,26 @@ var myComputeArray = function(valuesArray) {
     var par2 = valuesArray[1];
     var result = peso * ramdom * (par1+par2) ;
     return result | 0;
-}
+};
+
+var myComputeMap = function(valuesMap) {
+    var par1 = valuesMap["par1"];
+    var par2 = valuesMap["par2"];
+    var ramdom = Math.random();
+    var peso = 2;
+    var result = peso * ramdom * (par1+par2) ;
+    return result | 0;
+};
+
+
+var fun2 = function(mapValues) {
+    print("JS Class Definition: " + Object.prototype.toString.call(mapValues));
+    var par1 = mapValues.get("par1");
+    var par2 = mapValues.get("par2");
+    var ramdom = Math.random();
+    for each (var e in mapValues.keySet()) print(e);
+    var peso = 2;
+    var result = peso * ramdom * (par1+par2) ;
+    return result | 0;
+
+};
